92799ed264e8544f622e54dcf30a6080f6291404
Tousif Tamboli <tousiftamboli3@gmail.com>
2024-08-26 02:10:49 +0530

    navbar done with mobile responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
+import MobileNav from './MobileNav/MobileNav';
 
 const Navbar = () => {
+
+    const[openMenu, setOpenMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setOpenMenu(!openMenu);
+    };
+
   return (
+    <>
+    <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
+    
     <nav className='nav-wrapper'>
       <div className='nav-content'>
         <img className='logo' src="./assets/images/logo.svg" alt='Logo' />
@@ -23,8 +34,17 @@ const Navbar = () => {
             <button className='contact-btn' onClick={() => {}}>Hire Me</button>
           </li>
         </ul>
+
+        <button className='menu-btn' onClick={(toggleMenu)}>
+            <span
+            className={'material-symbols-outlined'}
+            style={{fontSize: "1.8rem"}}>
+                {openMenu ? "close" : "menu"}
+            </span>
+        </button>
       </div>
     </nav>
+    </>
   );
 };
 
