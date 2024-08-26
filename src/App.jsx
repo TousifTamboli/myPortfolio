9bd4d56a9676efcad8e864de1a2777adbeb00415
Tousif Tamboli <tousiftamboli3@gmail.com>
2024-08-26 15:19:11 +0530

    hero section checkpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Navbar/Hero/Hero';
 
 function App() {
   return (
     <>
       <Navbar />
+      <div className='container'>
+        <Hero />
+      </div>
     </>
   );
 }
