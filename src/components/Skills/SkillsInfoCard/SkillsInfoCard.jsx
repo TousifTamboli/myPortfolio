b1fdb9b00c4c07e085c5846169e2d99c02f9cbb5
Tousif Tamboli <tousiftamboli3@gmail.com>
2024-08-26 17:36:59 +0530

    skills section complete

--- src/components/Skills/SkillsInfoCard/SkillsInfoCard.jsx
+++ src/components/Skills/SkillsInfoCard/SkillsInfoCard.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import './SkillsInfoCard.css';
 
-const SkillsInfoCard = ({ heading, skills }) => {  // Destructure props correctly
+const SkillsInfoCard = ({ heading, skills }) => {
   return (
     <div className='skills-info-card'>
       <h6>{heading}</h6>
       
+      <div className='skills-info-content'>
+        {skills.map((item, index) => (
+            <React.Fragment key={`skills_${index}`}>
+                <div className='skills-info'>
+                    <p>{item.skill}</p>
+                    <p className='percentage'>{item.percentage}</p>
+                </div>
+
+                <div className='skills-progress-bg'>
+                  <div
+                    className='skills-progress'
+                    style={{ width: item.percentage }}
+                  ></div>
+                </div>
+            </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 };
